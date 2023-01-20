c9247dec67014138e7f8935c61551e1a6a3862ce
Marcela B Petroli <marcelabpetroli@gmail.com>
2023-01-20 13:14:50 +0100

    Edit styles of character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -10,14 +10,17 @@ const CharacterDetail = (props) => {
     if (characterSelected !== undefined) {
       return (
         <section className='details'>
-          <h1 className='details__title'>Character Details!</h1>
+          {/* From: https://codepen.io/wheresdara/pen/wvXBpwa */}
+          <div className='search__stars'></div>
+          {/* From: https://codepen.io/wheresdara/pen/wvXBpwa */}
+          <h1 className='details__title'>Character Details</h1>
           <li className='details__list'>
             <article className='details__article'>
               <h2 className='details__name'>{characterSelected.name}</h2>
               <div className='details__container'>
-                <div className='main__content-image-container'>
+                <div className='details__content-image-container'>
                   <img
-                    className='main__content-image-characters'
+                    className='details__content-image-characters'
                     src={characterSelected.image}
                     alt={characterSelected.name}
                     title={`Photo of ${characterSelected.name}`}
@@ -26,7 +29,9 @@ const CharacterDetail = (props) => {
                 <p className='details__sub'>Species: {characterSelected.species}</p>
                 <p className='details__sub'>Origin: {characterSelected.origin}</p>
                 <p className='details__sub'>Status: {characterSelected.status}</p>
-                <p className='details__sub'>Appears in {characterSelected.episode} episodes</p>
+                <p className='details__sub'>
+                  Appears in <span className='details__sub-episodes'>{characterSelected.episode}</span> episodes
+                </p>
               </div>
             </article>
             <Link className='details__link' to='/'>
